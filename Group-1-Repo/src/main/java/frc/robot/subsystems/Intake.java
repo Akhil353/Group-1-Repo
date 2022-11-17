828de9ea75d5bcacc20e4460e59b54783d0ee09d
KinetekEnergy <transyt8@gmail.com>
2022-11-16 17:10:07 -0800

    Update Intake.java

--- Group-1-Repo/src/main/java/frc/robot/subsystems/Intake.java
+++ Group-1-Repo/src/main/java/frc/robot/subsystems/Intake.java
@@ -5,7 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -27,7 +27,7 @@ public class Intake extends SubsystemBase {
     DoubleSolenoid DoubleSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 2);
     
     //default solenoid state for both solenoids
-    DoubleSolenoid1.set(Value.kOff); 
+    DoubleSolenoid1.set(Value.kOff);
     DoubleSolenoid2.set(Value.kOff);
 
     //extend and retract functions for both solenoids

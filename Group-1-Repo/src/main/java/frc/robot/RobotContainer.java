898489ce08c77d9aa123249b11d4e696c401c8df
KinetekEnergy <transyt8@gmail.com>
2022-11-29 16:20:42 -0800

    mapped a button to extend

--- Group-1-Repo/src/main/java/frc/robot/RobotContainer.java
+++ Group-1-Repo/src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 //imports for subsystem and commands;
 import frc.robot.commands.Extend;
 import frc.robot.commands.Retract;
@@ -52,8 +53,8 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     XboxController pilot = new XboxController(0); // create the controller w/the port
-    // JoystickButton A = new JoystickButton(pilot, Button.kA.Value);
-    // A.whenHeld(new )
+    JoystickButton A = new JoystickButton(pilot, Button.kA.Value); //use the A button
+    A.whenPressed(extend);
 
     }
 
